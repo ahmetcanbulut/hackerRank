62531c2fef78c45afea8a520348530b1c8b6154f
ahmetcanbulut <ahmetcanbulut@gmail.com>
2017-02-15 02:59:46 +0400

    Create MergeSortedLinkedLists.cpp

---
+++ MergeSortedLinkedLists.cpp
@@ -0,0 +1,50 @@
+/*
+  Merge two sorted lists A and B as one linked list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+
+
+Node* MergeLists(Node *headA, Node* headB)
+{
+  if(headA == NULL){
+      return headB;
+  }
+   
+  if(headB == NULL){
+      return headA;
+  }
+   
+  Node* node1 = headA;
+  Node* node2 = headB;
+    
+  while(node1 != NULL && node2 != NULL ){
+      Node* smaller;
+      Node* bigger;
+      if(node1->data < node2->data){
+          smaller = node1;
+          bigger = node2;
+      }else{
+          smaller = node2;
+          bigger = node1;
+      }
+      
+      while(smaller->next != NULL && smaller->next->data < bigger->data){
+          smaller = smaller->next;
+      }
+      
+      node1 = smaller->next;
+      smaller->next = bigger;
+      node2 = bigger;
+  }
+  
+  if(headA->data < headB->data){
+      return headA;
+  }else{
+      return headB;
+  }
+}
