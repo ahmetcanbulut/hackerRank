ee39cb79a376f46296238c581373fda9faf64f6c
ahmetcanbulut <ahmetcanbulut@gmail.com>
2017-02-18 03:46:13 +0400

    Create printTopViewOfBinaryTree.java

---
+++ printTopViewOfBinaryTree.java
@@ -0,0 +1,40 @@
+/*
+   class Node 
+       int data;
+       Node left;
+       Node right;
+*/
+
+void print_left(Node root){
+    if(root == null){
+        return;
+    }
+    
+    if(root.left != null){
+        print_left(root.left);
+    }
+    System.out.print(root.data + " ");
+}
+
+void print_right(Node root){
+    if(root == null){
+        return;
+    }
+    
+    System.out.print(root.data + " ");
+    if(root.right != null){
+        print_right(root.right);
+    }
+}
+
+void top_view(Node root)
+{
+    if(root == null){
+        return;
+    }
+    
+    print_left(root.left);
+    System.out.print(root.data + " ");
+    print_right(root.right);
+}
+
