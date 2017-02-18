3715a9c5c4a1d2ef47fbf31da0792dc12360e194
ahmetcanbulut <ahmetcanbulut@gmail.com>
2017-02-18 20:52:40 +0400

    Create LevelOrderTraversal.java

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,40 @@
+   /* 
+    
+    class Node 
+       int data;
+       Node left;
+       Node right;
+   */
+   void LevelOrder(Node root)
+   {
+       if(root == null){
+           return;
+       }
+       
+       int curLevelCount = 1;
+       int nextLevelCount = 0;
+       ArrayDeque<Node> queue = new ArrayDeque<Node>();
+
+       queue.push(root);
+       while(!queue.isEmpty()){
+           Node curNode = queue.peekFirst();
+           queue.pop();
+           curLevelCount--;
+           if(curNode != null){
+               System.out.print(curNode.data + " ");
+               if(curNode.left != null){
+                   queue.offer(curNode.left);
+                   nextLevelCount++;
+               }
+               if(curNode.right != null){
+                    queue.offer(curNode.right);
+                   nextLevelCount++;
+               }
+           }
+           if(curLevelCount == 0){
+               // System.out.println();
+               curLevelCount = nextLevelCount;
+               nextLevelCount = 0;
+           }
+       } 
+   }
